9d6908545b65b8ea20c81e2d4e1f8b7c024a32d4
giacomogaglione <gaglione.giacomo@gmail.com>
2021-03-14 00:27:35 +0100

    init

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -4,12 +4,11 @@ import { Heading, Text, BaseLayout } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
 import Page from 'components/layout/Page'
 import FarmStakingCard from 'views/Home/components/FarmStakingCard'
-import LotteryCard from 'views/Home/components/LotteryCard'
 import CakeStats from 'views/Home/components/CakeStats'
 import TotalValueLockedCard from 'views/Home/components/TotalValueLockedCard'
 import EarnAPYCard from 'views/Home/components/EarnAPYCard'
 import EarnAssetCard from 'views/Home/components/EarnAssetCard'
-import WinCard from 'views/Home/components/WinCard'
+
 
 const Hero = styled.div`
   align-items: center;
@@ -91,12 +90,10 @@ const Home: React.FC = () => {
       <div>
         <Cards>
           <FarmStakingCard />
-          <LotteryCard />
         </Cards>
         <CTACards>
           <EarnAPYCard />
           <EarnAssetCard />
-          <WinCard />
         </CTACards>
         <Cards>
           <CakeStats />
